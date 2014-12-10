b5034466d1e31711a9e94cfe0bac6507348563c8
fakhruddinb <fakhrudin.bh.net@gmail.com>
2014-12-10 16:45:32 +0530

    App

---
+++ MobileRecruiter/App.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Reflection;
+using System.Linq;
+
+namespace MobileRecruiter
+{
+	public class ResourceManager: Object
+	{
+		private static string _baseResource;
+		public static string BaseResource
+		{
+			get
+			{
+				return _baseResource ?? (_baseResource = Assembly.GetExecutingAssembly().FullName.Split(',').FirstOrDefault());
+			}
+		}
+
+		public static ImageSource PlatformImageResource(string resource)
+		{
+			return ImageSource.FromResource(BaseResource + "." + resource);
+		}
+
+	}
+
+	public class CheckedBox: Image
+	{
+		public bool flag { get; set;}
+
+		public CheckedBox()
+		{
+			flag = false;
+		}
+	}
+
+	public class VerticalStack: StackLayout
+	{
+		public VerticalStack()
+		{
+			Orientation = StackOrientation.Vertical;
+			Spacing = 10;
+			Padding = new Thickness (10);
+		}
+	}
+
+	public class HorizontalStack: StackLayout
+	{
+		public HorizontalStack()
+		{
+			Orientation = StackOrientation.Horizontal;
+			Spacing = 5;
+		}
+	}
+	public class TextField: Entry
+	{
+		public TextField()
+		{
+			WidthRequest = 175;
+		}
+	}
+
+	public class TextLabel: Label
+	{
+		public TextLabel()
+		{
+			WidthRequest = 120;
+			YAlign = TextAlignment.Center;
+			if (Device.Idiom == TargetIdiom.Phone)
+				Font = Font.SystemFontOfSize (14);
+			else
+				Font = Font.SystemFontOfSize (16);
+		}
+	}
+	public class App
+	{
+
+
+		public static Page GetMainPage ()
+		{
+			return new NavigationPage (new SignUpPage ());
+		
+		}
+
+
+
+	}
+}
+
---
+++ MobileRecruiter/NetworkCalls.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+
+namespace MobileRecruiter
+{
+	public class NetworkCalls
+	{
+		public NetworkCalls ()
+		{
+
+		}
+		private SignUpPage callerPage { get; set;}
+		public void SignUp (string userName, SignUpPage signUpPage)
+		{
+			callerPage = signUpPage;
+			UriBuilder signUpURI = new UriBuilder ("http://134.213.136.240:1081/test.htm");
+			signUpURI.Query = "&id=" + WebUtility.HtmlEncode (userName) + WebUtility.HtmlEncode("&DeviceType=Android&DeviceOS=Android 4.4&DeviceMake=HTC&FirstName=Hal 3&LastName=Bent&Phone=11111111&AgencyName=Agent C Name&AdditionalInfo=No Info");
+			HttpWebRequest signUpRequest = (HttpWebRequest)WebRequest.Create (signUpURI.Uri);
+
+			using (HttpWebResponse response = signUpRequest.GetResponse () as HttpWebResponse) 
+			{
+				if (response.StatusCode != HttpStatusCode.OK)
+					Console.WriteLine (response.StatusCode);
+				else 
+				{
+					this.callerPage.UpdateUI (response.StatusCode.ToString());
+				}
+			}
+//			
+		}
+
+
+
+	}
+}
+
